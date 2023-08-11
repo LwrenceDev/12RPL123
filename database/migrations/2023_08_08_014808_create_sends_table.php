a600487965594733eb032c33f8430baa26ddd48b
LwrenceDev <evntrst@gmail.com>
2023-08-11 08:28:32 +0700

    okok

--- database/migrations/2023_08_08_014808_create_sends_table.php
+++ database/migrations/2023_08_08_014808_create_sends_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('sends', function (Blueprint $table) {
             $table->id();
+            $table->string('no_agenda',50);
+            $table->string('jenis_surat',50);
+            $table->date('tanggal_kirim');
+            $table->string('no_surat', 50);
+            $table->string('pengirim', 50);
+            $table->string('perihal', 255);
+            $table->foreignId('id_user');
             $table->timestamps();
         });
     }
